7f60c8b207cd4969f36f95d8cfc9eb5de7d4e448
unknown <pabloechegaray95@gmail.com>
2020-11-30 00:16:31 -0300

    Evento de click a las tarjetas y cambio h1 de manera dinamica

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+//=====================================
+//Efecto marquesina el en span "envios a todo el pais"
 let texto = "¡ Envios - a - todo - el - pais ! - Comunicate - al - 1111111111";
+
 let span = document.querySelector("#containSlide").childNodes[3];
 console.log(span);
 span.innerText = "";
@@ -15,7 +18,8 @@ let intervalo = setInterval(() => {
     clearInterval(intervalo);
   }
 }, 200);
-
+//======================================
+//Funcionalidad boton hamburgesa
 let menu = document.querySelector(".material-icons");
 let drawer = document.querySelector("#drawer");
 let contador = 0;
@@ -40,7 +44,7 @@ function cambiar() {
 }
 
 //=================================
-
+//boton scroll
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 101) {
     document.querySelector(".go-top-container").classList.add("show");
@@ -54,3 +58,46 @@ document.querySelector(".go-top-container").addEventListener("click", () => {
     top: 0,
   });
 });
+
+//====================================
+// Modal y evento de click en tarjetas de productos
+
+let tarjetas = document.querySelectorAll(".tarjeta");
+let modal = document.querySelector("#modal");
+let btn_cerrar = document.getElementById("cerrarModal");
+
+tarjetas.forEach((tarjeta) => {
+  tarjeta.addEventListener("click", () => {
+    modal.style.display = "flex";
+    console.dir(tarjeta);
+    let texto_interno = tarjeta.innerText;
+    switch (texto_interno) {
+      case "Red Tejida":
+        cambiarH1(texto_interno);
+        break;
+      case "Red Extruida":
+        cambiarH1(texto_interno);
+        break;
+      case "Red Protectora":
+        cambiarH1(texto_interno);
+        break;
+      case "Red para Ajo":
+        cambiarH1(texto_interno);
+        break;
+      case "Precintos":
+        cambiarH1(texto_interno);
+        break;
+      case "Esponja SPA":
+        cambiarH1(texto_interno);
+        break;
+    }
+  });
+});
+
+btn_cerrar.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+function cambiarH1(texto) {
+  document.querySelector("#imagenes").childNodes[3].innerText = texto;
+}
